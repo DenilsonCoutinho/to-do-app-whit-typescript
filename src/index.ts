5a5912b71cea7ecb74c26bd860735692ffcbc257
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2024-02-05 05:14:01 -0300

    ajustando ui

--- src/index.ts
+++ src/index.ts
@@ -42,60 +42,88 @@ function openModal(id?: number): void {
         modal2.classList.add('hidden');
     }
 }
-
-function viewCurrentToDo() {
-    const currentPlans: NodeListOf<HTMLElement> | any = document.querySelector('.allPlans')
-    currentPlans.innerHTML = list.map((i: any) => {
-        return `
-        <div key={i.title} class="w-full max-w-[300px] bg-white hover:duration-300 hover:scale-105 mt-5 p-5 border-white  rounded-lg">
-        <h1 class="font-medium">${i.title}</h1>
-        <div class="flex flex-col">
-        <div class="flex justify-between">
-        <div class="flex items-center">
-        <lord-icon
-        src="https://cdn.lordicon.com/uecgmesg.json"
-        trigger="loop"
-        style="width:20px;height:20px">
-        </lord-icon>
-        <p class="text-xs text-gray-400">Progresso</p>
-        </div>
-        <p class="text-xs font-semibold">${i.did}/${i.toDid}</p>
-        </div>
-        <div class=" h-1 w-full relative bg-gray-400 rounded-xl my-3">
-        <div class="h-1 w-[${(i.did / i.toDid) * 100}%] absolute bg-green-500 rounded-xl duration-150"></div>
-        </div>
-        </div>
-        <div class="bg-gray-100 rounded-2xl p-1 w-20">
-        <h1 class="text-xs text-gray-600">${i.date}</h1
-        </div>
-        </div>
-        </div>
-    `
-    }).join('');
-}
-viewCurrentToDo()
-
+addEventListener
 let subTasks: NodeListOf<HTMLElement> | any = document.querySelector('.addSubTasks')
 let newInputTask: NodeListOf<HTMLElement> | any = document.querySelector('.newInputTask')
 let task: NodeListOf<HTMLElement> | any = document.querySelector('.task')
 let buttonAdd: NodeListOf<HTMLElement> | any = document.querySelector('.buttonAdd')
+let lengthZero: NodeListOf<HTMLElement> | any = document.querySelector('.whileLengthZero')
 let listSubTasks: any[] = []
 let objectData: any[] = []
 let allDataTask: any[] = []
+
+function observerTask() {
+    console.log('aqui', allDataTask)
+    if (allDataTask.length > 0) {
+        lengthZero.classList.add('hidden')
+    }
+}
+
+function editTask(){
+    
+}
+function viewCurrentToDo() {
+    const currentPlans: NodeListOf<HTMLElement> | any = document.querySelector('.allPlans')
+    document.querySelector('.to-do').innerHTML = allDataTask.length.toString()
+    currentPlans.innerHTML = allDataTask.map((i: any) => {
+        let taskDone = i.subTasks.filter(task =>  task.done !== false)
+        console.log('done',taskDone)
+        return `
+        <div key={i.title} class="w-full max-w-[300px] cursor-pointer bg-white hover:duration-300 hover:scale-105 mt-5 p-5 border-white  rounded-lg">
+                    <h1 class="font-medium">${i.title}</h1>
+                    <div class="flex flex-col">
+                        <div class="flex justify-between">
+                            <div class="flex items-center">
+                                <lord-icon src="https://cdn.lordicon.com/uecgmesg.json" trigger="loop"style="width:20px;height:20px">
+                                </lord-icon>
+                                <p class="text-xs text-gray-400">Progresso</p>
+                            </div>
+                            <p class="text-xs font-semibold">${taskDone.length}/${i.subTasks.length}</p>
+                        </div>
+                        <div class=" h-1 w-full relative bg-gray-400 rounded-xl my-3">
+                            <div class="h-1 w-[${( taskDone.length/i?.subTasks.length ) * 100}%] absolute bg-green-500 rounded-xl duration-150"></div>
+                        </div>
+                    </div>
+                <div class="bg-gray-100 rounded-2xl p-1 w-20">
+                     <h1 class="text-xs text-gray-600">${i.date}</h1
+                </div>
+                </div>
+        </div>
+         `
+    }).join('')
+
+        ;
+}
+
+viewCurrentToDo()
+
+
 function saveTask(): void {
     let onlyObjects = objectData.filter(item => typeof item === 'object');
-    allDataTask.push({
-        title: task.value,
-        subTasks: onlyObjects
-    })
-    console.log('aqui - 2 ', allDataTask)
+    if (task.value === '') {
+        alert('Tá vazio')
+    } else {
+        allDataTask.push({
+            id: Math.random(),
+            title: task.value,
+            subTasks: onlyObjects,
+            done: false,
+            date:date
+        })
+    }
+    objectData = []
+    console.log("aq", objectData)
+    observerTask()
+    viewCurrentToDo()
+    viewCurrentSubTask()
 }
 
 function addInput(): void {
     idWithTime++;
     let object: any = {
         id: idWithTime,
-        subTasks: ''
+        subTasks: '',
+        done: true,
     }
     objectData.push(object)
     console.log(objectData)
@@ -119,7 +147,6 @@ function updateSubTasks(id: number, value: string): void {
     if (index !== -1) {
         objectData[index].subTasks = value;
     }
-    console.log(objectData);
 }
 
 function removeData(obj: number): void {
